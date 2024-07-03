c1bc71c254881e04ce03317a5ea1174b447f7057
vanshchinda <vanshchinda@gmail.com>
2024-07-03 15:58:39 +0530

    bug: auth error fixed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
